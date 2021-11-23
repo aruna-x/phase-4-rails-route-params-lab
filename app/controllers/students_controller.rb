e5eda42dc12d34466ede534201a464e624b50c6a
Aruna <aruna.evan15@gmail.com>
2021-11-23 11:46:57 -0800

    commit tree aaea6174211a93bf34a2043df02a04be48c0a86a

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,16 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    name = (params[:name] ? params[:name].camelize : nil)
+    f_name = Student.find_by(first_name: name)
+    l_name = Student.find_by(last_name: name)
+    students = (!name ? Student.all : (f_name ? [{first_name: f_name.first_name, last_name: f_name.last_name, grade: f_name.grade}] : [{first_name: l_name.first_name, last_name: l_name.last_name, grade: l_name.grade}]))
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
